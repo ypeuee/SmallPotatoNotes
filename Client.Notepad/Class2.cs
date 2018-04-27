b2310c85856226313170dc73e9e96441213874b6
ypeuee <ypeuee@163.com>
2018-04-27 14:11:25 +0800

    正在添加统计功能

---
+++ Client.Notepad/Class2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Windows;
+using Client.Pub.ServiceProxyFactoryLib;
+
+namespace Client.Notepad
+{
+    class Class2
+    {
+        private void Logig()
+        {
+            //MUserInfoLogin user = new MUserInfoLogin();
+            //user.Code = TxtCode.Text.Trim();
+            //user.Password = TxtPassword.Text;
+
+            //IMessage iMessage = SPF.Create<IMessage>("YpeuEe.ServerService.Message.SMessage", RCT.NoneMode, SMessageCallback.MessageCallback);
+
+            //try
+            //{
+            //    Class1.UserInfo = iMessage.Login(user);
+
+            //    MessageBox.Show(Class1.UserInfo.Message);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
+
+        }
+    }
+}
